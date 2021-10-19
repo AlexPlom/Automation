0841c43a4ebfafa6950d424cde1e4f1ba060c3dd
alexplom <alexiosplomaritis@gmail.com>
2021-10-19 21:20:43 +0300

    Batman commit

---
+++ Automation/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PuppeteerSharp;
+
+Console.WriteLine("Hello, World!");
+
+string outputFile = "./gg.txt";
+await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    Headless = true
+});
+var page = await browser.NewPageAsync();
+await page.GoToAsync("http://www.google.com");
+await page.ScreenshotAsync(outputFile);
